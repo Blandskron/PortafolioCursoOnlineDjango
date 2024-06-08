b98b5f8ecc8abdc57a3ed47ccc9351933022b7c3
blandskron <bastianlandskronfreelancer@gmail.com>
2024-06-07 21:54:20 -0400

    ajuste en url para carga de imagenes por basededatos

--- portafolio/portafolio/urls.py
+++ portafolio/portafolio/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,7 @@ urlpatterns = [
     path('home/', include('homeapp.urls')),
     path('auth/', include('authapp.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
