490f5b5e3fc1719fdebbd984fb4fe70f9743f1eb
Becerra-Seba <sebastian.becerra@slepcolchagua.cl>
2024-06-11 20:33:00 -0400

    actualizar2

---
+++ portafolio/blogapp/contactapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'message': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+        }
